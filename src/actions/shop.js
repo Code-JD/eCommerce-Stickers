a13caaa793a0b4a857d6b276a3b51aab964b2c04
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-05-22 16:49:08 -0700

    fetch products action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,12 +1,21 @@
 import {
-	// SET_SHOP_CATEGORIES
+	SET_SHOP_CATEGORIES,
 	SET_NAVBAR_LINKS,
-	SET_SHOP_PRODUCTS
+	SET_SHOP_PRODUCTS,
+	FILTER_PRODUCTS_WITH_CATEGORY_ID
 } from './types';
+
+export function filterProductsWithCategoryId(_id) {
+	console.log(_id, 'jello');
+	return ({
+		type: FILTER_PRODUCTS_WITH_CATEGORY_ID,
+		payload: _id
+	})
+}
 
 export function fetchShopCategories() {
 	return ({
-		type: SET_NAVBAR_LINKS,
+		type: SET_SHOP_CATEGORIES,
 		payload: [
 			{
 				_id: 0,
@@ -37,6 +46,7 @@ export function fetchShopCategories() {
 				title: 'Ruby'
 			}
 		]
+		
 	})
 }
 
